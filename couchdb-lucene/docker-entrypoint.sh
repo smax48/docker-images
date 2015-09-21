41636c00518dce049304d409554e6611047c4eb7
smax48 <smax48@gmail.com>
2015-09-21 16:34:50 +1000

    added $CDB_AUTH for couchdb-lucene authorisation (default empty, use docker run to set a value)

--- couchdb-lucene/docker-entrypoint.sh
+++ couchdb-lucene/docker-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+sed -e 's/localhost:5984/'"$CDB_AUTH"'couchdb:5984/' -i /opt/dist/couchdb-lucene-$CDB_LUCENE_VER/conf/couchdb-lucene.ini
+
 /opt/dist/couchdb-lucene-$CDB_LUCENE_VER/bin/run
